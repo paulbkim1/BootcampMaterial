32e5a0b460f082e8bbbd44beabaed1744da21dd1
paulbkim1 <paulbkim1@gmail.com>
2022-08-23 14:30:10 -0700

    project

---
+++ 03-js/Projects/Making_Connections/profile-page/script.js
@@ -0,0 +1,18 @@
+function editname(namechange) {
+    var nameid = document.querySelector("#" + namechange);
+    nameid.innerText = "Paul Kim";
+}
+
+var connectionrequest = document.querySelector("#requests")
+var connectionsamount = document.querySelector("#connections")
+
+function add(addid) {
+    document.querySelector("#" + addid).remove();
+    connectionsamount.innerText++;
+    connectionrequest.innerText--;
+}
+
+function remove(addid) {
+    document.querySelector("#" + addid).remove();
+    connectionrequest.innerText--;
+}
